38cc103e38ef6a85659e2620d3967a2e15619f89
Edwin de Jonge <edwindjonge@gmail.com>
2021-06-01 17:14:37 +0200

    renaming

--- R/run_v_to_c.R
+++ R/run_v_to_c.R
@@ -4,7 +4,7 @@ run_v_to_c <- function(dir="./src", pkg="vtest", ...){
   on.exit(setwd(old_wd))
   r_mod <- system.file("v", package="rvee")
   command <- "v"
-  #TODO set VCACHE
+  #TODO set VCACHE or copy r mod to /tmp dir.
   args = c( "-o"   , sprintf("%s.c", pkg)
           , "-shared"
           , "-path", shQuote(sprintf("%s|@vlib|@vmodules", r_mod))
